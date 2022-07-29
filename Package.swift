2c2c08441294bb1f5de6474923c7822aa87d4049
Alexandre H. Saad <alexandresaad@icloud.com>
2022-07-29 13:34:10 +0100

    Commit
    Added Plugin

--- Package.swift
+++ Package.swift
@@ -1,28 +1,26 @@
-// swift-tools-version: 5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.6
 
 import PackageDescription
 
-let package = Package(
-    name: "git-contributors",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "git-contributors",
-            targets: ["git-contributors"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "git-contributors",
-            dependencies: []),
-        .testTarget(
-            name: "git-contributorsTests",
-            dependencies: ["git-contributors"]),
-    ]
+let package: Package = .init(
+	name: "swift-contributors-plugin",
+	defaultLocalization: "en",
+	products: [
+		.plugin(name: "Contributors", targets: ["Contributors"])
+	],
+	targets: [
+		.plugin(
+			name: "Contributors",
+			capability: .command(
+				intent: .custom(
+					verb: "generate-contributors",
+					description: "Generates the CONTRIBUTORS.txt file based on Git logs."
+				),
+				permissions: [
+					.writeToPackageDirectory(reason: "This command writes the new CONTRIBUTORS.txt file.")
+				]
+			)
+		)
+	],
+	swiftLanguageVersions: [.v5]
 )
--- Sources/git-contributors/git_contributors.swift
+++
@@ -1,6 +0,0 @@
-public struct git_contributors {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
